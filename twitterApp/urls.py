c7417c242b7cdf3c3275536ddc363b45f2d4b7a1
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-18 18:37:48 -0400

    doubting deployment

--- twitterApp/urls.py
+++ twitterApp/urls.py
@@ -31,8 +31,11 @@ urlpatterns = [
     # path('twitter_callback/', views.twitter_callback, name='twitter_callback'),
     # path('twitter_logout/', views.twitter_logout, name='twitter_logout'),
     
-    path('authorization/', include('authorization.urls')),
-    
+    # path('authorization/', include('authorization.urls')),
+    path('', views.index, name='index'),
+    path('twitter_login/', views.twitter_login, name='twitter_login'),
+    path('twitter_callback/', views.twitter_callback, name='twitter_callback'),
+    path('twitter_logout/', views.twitter_logout, name='twitter_logout'),
 ]
 # from django.urls import path
 # from . import views
