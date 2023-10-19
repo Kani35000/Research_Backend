6c17c2d2433e595a4b90080415017d1865e8a720
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-18 20:26:25 -0400

    doubting deployment

--- twitterApp/urls.py
+++ twitterApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from authorization import views
+# from authorization import views
 #import userLoginApp.views
 # from userLoginApp import views
 
@@ -23,7 +23,7 @@ from authorization import views
 urlpatterns = [
     path('authorization/', include('authorization.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     # path('', views.home),
     # path('submitform', views.submitform),
     # path('aboutresearch/', views.aboutresearch, name='aboutresearch'),
