a29582424db06a9afae31c1023e067bdb32c2b0d
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-19 09:17:07 -0400

    doubting deployment

--- twitterApp/urls.py
+++ twitterApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from authorization import views
+from authorization import views
 #import userLoginApp.views
 # from userLoginApp import views
 
