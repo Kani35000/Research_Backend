a2e733a4ed6e08a0f6c7c5463e4ae27d8a71984f
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-19 12:19:45 -0400

    doubting deployment

--- authorization/urls.py
+++ authorization/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', views.index2, name='index2'),
+    path('index/', views.index2, name='index2'),
     path('twitter_login/', views.twitter_login, name='twitter_login'),
     path('twitter_callback/', views.twitter_callback, name='twitter_callback'),
     path('twitter_logout/', views.twitter_logout, name='twitter_logout'),
