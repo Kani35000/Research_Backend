5b9f1ef3eff82b20f327f8d5a1509a980ffed4fc
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-18 20:14:55 -0400

    doubting deployment

--- twitterApp/settings.py
+++ twitterApp/settings.py
@@ -111,6 +111,19 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'research2db',
+#         'USER': 'kani',
+#         'PASSWORD':'TDjakes35',
+#         'HOST': 'localhost',
+#         'PORT':'5432'
+
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
