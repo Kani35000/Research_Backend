796e51560bb3328fdf555fd5a6d54a6d6b81705a
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-19 11:58:53 -0400

    doubting deployment

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,9 +1,9 @@
-from django.urls import paths, include
+from django.urls import paths
 from . import view
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index/', include('twitterApp.urls')),
+    # path('index/', include('twitterApp.urls')),
     path('twitter_login/', views.twitter_login, name='twitter_login'),
     path('twitter_callback/', views.twitter_callback, name='twitter_callback'),
     path('twitter_logout/', views.twitter_logout, name='twitter_logout'),
