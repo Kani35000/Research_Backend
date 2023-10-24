3ac1e9be991a5ab6fc98b63242fb59a5a4c2abb9
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-10-24 14:15:20 -0400

    adding timeline link

--- authorization/views.py
+++ authorization/views.py
@@ -79,7 +79,7 @@ def twitter_callback(request):
                     # api_url = "https://api.twitter.com/2/tweets"
                     # response = requests.get(api_url, headers= headers)
                     # twitter_data = response.json()
-                return render(request, 'authorization/home.html', {'user': user, 'data': tweet} )
+                return render(request, 'authorization/home.html', {'user': user} )
                     
             else:
                 messages.add_message(request, messages.ERROR, 'Unable to get profile details. Please try again.')
